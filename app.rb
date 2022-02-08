1fc29976987c8e6b4b41aaae9c6d6f331e189aee
Charlotte Buchan <cebuc@talktalk.net>
2022-02-08 16:31:37 +0000

    step 14 completed

--- app.rb
+++ app.rb
@@ -6,11 +6,18 @@ get '/random-cat' do
   erb :index  # method from templating system that renders file called index.erb
 end
 
-get '/named-cat' do
-  p params[:name]
+get '/cat-form' do
+  erb :cat_form
+end 
+
+post '/named-cat' do
+  p params
   @name = params[:name]
   erb :index
 end
+
+
+
 
 get '/' do
   'hello world!'
